27d4dafe06e59ca67d59ba3f67443d7c0e16ec5f
thefather12 <94747184+thefather12@users.noreply.github.com>
2023-03-23 16:07:40 -0600

    UPDATE001

--- gestorssh/instpainel.sh
+++ gestorssh/instpainel.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 clear
-echo "America/Sao_Paulo" > /etc/timezone
+echo "Centro America/El Salvador" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime > /dev/null 2>&1
 dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 clear
-echo -e "\E[44;1;37m    INSTALAR PAINEL PWEB SWIT-T     \E[0m" 
+echo -e "\E[44;1;37m    INSTALAR PANEL PWEB SHADOW     \E[0m" 
 echo ""
-echo -e "                 \033[1;31mBy @swittecnologia\033[1;36m"
+echo -e "                 \033[1;31mBy @THEFATHER12\033[1;36m"
 echo ""
-echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
+echo -ne "\n\033[1;32mDE UN ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
-echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
+echo -e "\n\033[1;36mINICIANDO INSTALACION \033[1;33mESPERE..."
 apt-get install figlet -y > /dev/null 2>&1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
@@ -33,9 +33,9 @@ wget https://github.com/thefather12/psshplus-/raw/main/gestorssh/updatepainelarm
 wget https://github.com/thefather12/psshplus-/raw/main/gestorssh/updatepainelarm18.sh > /dev/null 2>&1
 chmod 777 *.sh > /dev/null 2>&1
 clear
-echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
+echo -e "        \033[1;33m • \033[1;32mINSTALACION CONCLUIDA\033[1;33m • \033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
-echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@swittecnologia\033[0m"
+echo -e "\033[1;33m MAS INFORMACION \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@THEFATHER12\033[0m"
 cat /dev/null > ~/.bash_history && history -c
 exit
